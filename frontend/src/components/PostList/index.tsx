78c9533e8b9da22a16d9ac778cd76372bd5aef5f
Corey Burns <coreymburns@gmail.com>
2020-09-19 12:28:31 -0400

    fixing queries/mutations

--- frontend/src/components/PostList/index.tsx
+++ frontend/src/components/PostList/index.tsx
@@ -1,6 +1,5 @@
 import { NetworkStatus } from "@apollo/client"
-import { Box } from "@chakra-ui/core"
-import Post from "@components/PostList/Post"
+import { Box, Flex, Heading } from "@chakra-ui/core"
 import { usePostsQuery } from "@generated/graphql"
 import * as React from "react"
 
@@ -35,7 +34,23 @@ const PostList = () => {
       return (
         <ul>
           {allPosts.map((post, index) => (
-            <Post key={`Post(${index}-${post.title})`} post={post} />
+            <Flex
+              key={`post-${post.title}-${index}-${post.id}`}
+              p={5}
+              shadow="md"
+              borderWidth="1px"
+              minW="100%"
+            >
+              <Box>
+                <Heading fontSize="xl">Title - {post?.title ?? "404"}</Heading>
+                <Heading fontSize="md">
+                  Category - {post?.category.name ?? "Not Found"}
+                </Heading>
+                <Heading fontSize="sm">
+                  CreatedBy - {post?.author.username ?? "Not Found"}
+                </Heading>
+              </Box>
+            </Flex>
           ))}
         </ul>
       )
