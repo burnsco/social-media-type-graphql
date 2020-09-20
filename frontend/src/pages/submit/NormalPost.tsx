509801b7884d75ef368c7579b075533a761f2e0a
Corey Burns <coreymburns@gmail.com>
2020-09-20 13:49:39 -0400

    revamp forms, post creation working with select category, moved subreddit creation

--- frontend/src/pages/submit/NormalPost.tsx
+++ frontend/src/pages/submit/NormalPost.tsx
@@ -4,80 +4,35 @@ import {
   FormControl,
   FormErrorMessage,
   FormLabel,
-  Input,
-  useToast
+  Input
 } from "@chakra-ui/core"
-import { PostInput, useCreatePostMutation } from "@generated/graphql"
-import { Field, Form, Formik } from "formik"
-import { useRouter } from "next/router"
+import { Field, Form } from "formik"
 import * as React from "react"
 
 const SubmitRegularPost: React.FC = () => {
-  const toast = useToast()
-  const router = useRouter()
-  const [submitPost, { loading, error }] = useCreatePostMutation()
-
-  if (error) {
-    console.log(error)
-  }
-  if (loading) return null
-
-  function validateTitle(value: string) {
-    let error
-    if (!value) {
-      error = "Title is required"
-    } else if (value.length < 2) {
-      error = "Subreddit has to be at least 5 characters. "
-    }
-    return error
-  }
-
-  const handleSubmit = (values: PostInput) => {
-    submitPost({
-      variables: {
-        data: {
-          title: values.title,
-          categoryId: values.categoryId
-        }
-      }
-    })
-  }
-
   return (
     <Box>
-      <Formik
-        initialValues={{ title: "", categoryId: 2 }}
-        onSubmit={(values, actions) => {
-          setTimeout(() => {
-            actions.setSubmitting(false)
-            handleSubmit(values)
-          }, 1000)
-        }}
-      >
-        {props => (
-          <Form onSubmit={props.handleSubmit}>
-            <Field name="title" validate={validateTitle}>
-              {({ field, form }: any) => (
-                <FormControl
-                  isInvalid={form.errors.title && form.touched.title}
-                >
-                  <FormLabel htmlFor="title">Post Title</FormLabel>
-                  <Input {...field} id="title" placeholder="title" />
-                  <FormErrorMessage>{form.errors.title}</FormErrorMessage>
-                </FormControl>
-              )}
-            </Field>
-            <Button
-              mt={4}
-              colorScheme="teal"
-              isLoading={props.isSubmitting}
-              type="submit"
-            >
-              Submit
-            </Button>
-          </Form>
-        )}
-      </Formik>
+      {(props: any) => (
+        <Form onSubmit={props.handleSubmit}>
+          <Field name="title">
+            {({ field, form }: any) => (
+              <FormControl isInvalid={form.errors.title && form.touched.title}>
+                <FormLabel htmlFor="title">Post Title</FormLabel>
+                <Input {...field} id="title" placeholder="title" />
+                <FormErrorMessage>{form.errors.title}</FormErrorMessage>
+              </FormControl>
+            )}
+          </Field>
+          <Button
+            mt={4}
+            colorScheme="teal"
+            isLoading={props.isSubmitting}
+            type="submit"
+          >
+            Submit
+          </Button>
+        </Form>
+      )}
     </Box>
   )
 }
--- frontend/src/pages/submit/index.tsx
+++ frontend/src/pages/submit/index.tsx
@@ -1,76 +1,147 @@
 import {
+  Box,
+  Button,
   FormControl,
+  FormErrorMessage,
   FormLabel,
-  Heading,
+  Input,
   Select,
   Stack,
   Tab,
   TabList,
   TabPanel,
   TabPanels,
-  Tabs
+  Tabs,
+  useToast
 } from "@chakra-ui/core"
-import { useCategoriesQuery } from "@generated/graphql"
+import { useCategoriesQuery, useCreatePostMutation } from "@generated/graphql"
+import { Field, Form, Formik } from "formik"
+import { useRouter } from "next/router"
 import * as React from "react"
-import CreateImageOrVideoPost from "./ImageVideoPost"
-import CreateLinkPost from "./LinkPost"
-import CreateRegularPost from "./NormalPost"
-import CreateSubreddit from "./Subreddit"
+import * as Yup from "yup"
+
+interface CreatePostProps {
+  title: string
+  categoryId: number
+}
 
 const SubmitPage: React.FunctionComponent = () => {
-  const { data, loading, error } = useCategoriesQuery()
+  const toast = useToast()
+  const router = useRouter()
+  const {
+    data,
+    loading: loadingSubreddits,
+    error: subredditError
+  } = useCategoriesQuery()
 
-  if (loading) return null
+  const [submitPost, { loading, error }] = useCreatePostMutation()
 
+  if (loading || loadingSubreddits) return null
+  if (subredditError) {
+    console.log(subredditError)
+  }
   if (error) {
     console.log(error)
   }
 
+  const handleSubmit = (values: CreatePostProps) => {
+    submitPost({
+      variables: {
+        data: {
+          title: values.title,
+          categoryId: Number(values.categoryId)
+        }
+      }
+    })
+  }
+
   return (
-    <Stack spacing={5}>
-      <Heading size="lg" fontStyle="italic">
-        Create a Post
-      </Heading>
+    <Box>
+      <Stack spacing={5}>
+        <Formik
+          initialValues={{ title: "", categoryId: 1 }}
+          validationSchema={Yup.object().shape({
+            title: Yup.string().required("Required"),
+            categoryId: Yup.number().required("Required")
+          })}
+          onSubmit={(values: CreatePostProps, actions) => {
+            setTimeout(() => {
+              actions.setSubmitting(false)
+              handleSubmit(values)
+            }, 1000)
+          }}
+        >
+          {props => (
+            <Form>
+              <Field name="categoryId">
+                {({ field }: any) => (
+                  <FormControl id="categoryId">
+                    <Select
+                      {...field}
+                      isRequired
+                      placeholder="Choose a community"
+                      size="lg"
+                      width="50%"
+                    >
+                      {data?.categories.map(subreddit => (
+                        <option
+                          key={`subreddit-${subreddit.name}-sidemenu`}
+                          value={subreddit.id}
+                        >
+                          {subreddit.name}
+                        </option>
+                      ))}
+                    </Select>
+                  </FormControl>
+                )}
+              </Field>
 
-      <FormControl id="category">
-        <FormLabel htmlFor="category">Subreddit</FormLabel>
-        <Select placeholder="Choose a community" size="lg" width="50%">
-          {data?.categories.map(subreddit => (
-            <option
-              key={`subreddit-${subreddit.name}-sidemenu`}
-              value={subreddit.id}
-            >
-              {subreddit.name}
-            </option>
-          ))}
-        </Select>
-        <Tabs variant="enclosed">
-          <TabList>
-            <Tab>Post</Tab>
-            <Tab>Link</Tab>
-            <Tab>Images & Video</Tab>
-            <Tab>Subreddit</Tab>
-          </TabList>
+              <Tabs variant="enclosed">
+                <TabList>
+                  <Tab>Post</Tab>
+                  <Tab>Link</Tab>
+                  <Tab>Images & Video</Tab>
+                </TabList>
 
-          <TabPanels>
-            <TabPanel>
-              <CreateRegularPost />
-            </TabPanel>
+                <TabPanels>
+                  <TabPanel>
+                    <Field name="title">
+                      {({ field, form }: any) => (
+                        <FormControl
+                          isInvalid={form.errors.title && form.touched.title}
+                        >
+                          <FormLabel htmlFor="title">Post Title</FormLabel>
+                          <Input {...field} id="title" placeholder="title" />
+                          <FormErrorMessage>
+                            {form.errors.title}
+                          </FormErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
+                    <Button
+                      mt={4}
+                      colorScheme="teal"
+                      isLoading={props.isSubmitting}
+                      type="submit"
+                    >
+                      Submit
+                    </Button>
+                  </TabPanel>
 
-            <TabPanel>
-              <CreateLinkPost />
-            </TabPanel>
+                  <TabPanel>
+                    <div>Link</div>
+                  </TabPanel>
 
-            <TabPanel>
-              <CreateImageOrVideoPost />
-            </TabPanel>
-            <TabPanel>
-              <CreateSubreddit />
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-      </FormControl>
-    </Stack>
+                  <TabPanel>
+                    <div>Images & Video</div>
+                  </TabPanel>
+                </TabPanels>
+              </Tabs>
+            </Form>
+          )}
+        </Formik>
+      </Stack>
+    </Box>
   )
 }
 
