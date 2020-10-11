2f5abd0fa3f4d792b9405686a05af79bbe92ddc2
Corey Burns <coreymburns@gmail.com>
2020-10-11 18:21:35 -0400

    postlist test, more added

--- frontend/src/__tests__/PostList.test.tsx
+++ frontend/src/__tests__/PostList.test.tsx
@@ -1,43 +1,61 @@
 import PostList from "@/components/PostList"
 import { PostsDocument } from "@/generated/graphql"
 import { render } from "@/utils/test-utils"
+import { InMemoryCache } from "@apollo/client"
 import { MockedProvider } from "@apollo/client/testing"
 import "@testing-library/jest-dom"
 import { screen } from "@testing-library/react"
 import React from "react"
-import { posts } from "../utils/postData"
 
-const mocks = [
-  {
-    request: {
-      query: PostsDocument,
-      variables: {
-        skip: 0,
-        first: 4
+const posts = new InMemoryCache()
+posts.writeQuery({
+  query: PostsDocument,
+  variables: {
+    skip: 0,
+    first: 4
+  },
+  data: {
+    posts: {
+      __typename: "Post",
+      id: "1",
+      createdAt: "1601511422000",
+      updatedAt: "1601511422000",
+      title: "Post #1",
+      text: "Hello",
+      category: {
+        __typename: "Category",
+        id: "1",
+        name: "React"
       },
-      notifyOnNetworkStatusChange: true
-    },
-    result: {
-      data: {
-        posts: [...posts],
-        _allPostsMeta: {
-          __typename: "_QueryMeta",
-          count: 1
-        }
+      author: {
+        __typename: "User",
+        id: "1",
+        username: "Corey"
+      },
+      totalComments: {
+        count: 0
+      },
+      totalVotes: {
+        score: 1,
+        count: 0
       }
+    },
+    _allPostsMeta: {
+      count: 1
     }
   }
-]
+})
 
 describe("PostList", () => {
   it("renders without crashing", async () => {
     render(
-      <MockedProvider mocks={mocks}>
+      <MockedProvider cache={posts} addTypename={false}>
         <PostList />
       </MockedProvider>
     )
-    console.log(mocks)
+
     const loading = await screen.findByText(/no posts here/i)
+    screen.debug(loading)
     expect(loading).toBeInTheDocument()
   })
 })
