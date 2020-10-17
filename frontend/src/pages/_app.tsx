c61513d7e85b0b04657b43fafc7a5602b1e3ba85
Corey Burns <coreymburns@gmail.com>
2020-10-17 13:17:07 -0400

    few more tweaks..

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -5,19 +5,12 @@ import { ApolloProvider } from "@apollo/client"
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/core"
 import theme from "@chakra-ui/theme"
 import { AppProps } from "next/dist/next-server/lib/router/router"
-import Head from "next/head"
 
 export default function App({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps.initialApolloState)
 
   return (
     <>
-      <Head>
-        <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <link rel="icon" type="image/png" sizes="96x96" href="/favicon.png" />
-        <meta name="theme-color" content="#319795" />
-      </Head>
       <ApolloProvider client={apolloClient}>
         <ChakraProvider resetCSS theme={theme}>
           <ColorModeScript initialColorMode="dark" />
