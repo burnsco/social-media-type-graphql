61aa98ee2bae2ceb85ea8fb5cb9dc9d800a5d80b
Corey Burns <coreymburns@gmail.com>
2020-10-23 14:01:10 -0400

    added a postnotfound page, fixed the single post page

--- frontend/src/pages/r/[category]/[id].tsx
+++ frontend/src/pages/r/[category]/[id].tsx
@@ -1,53 +1,43 @@
 import CommentsPageWithData from "@/components/Comment/Data"
 import SubmitCommentForm from "@/components/Comment/Form"
 import NewPost from "@/components/Post"
-import { usePostQuery } from "@/generated/graphql"
-import { Box, Stack } from "@chakra-ui/core"
+import { PostNotFound } from "@/components/shared/PostNotFound"
+import { usePostLazyQuery } from "@/generated/graphql"
+import { Box, Skeleton, Stack } from "@chakra-ui/core"
 import { useRouter } from "next/router"
-import { useEffect, useState } from "react"
+import React, { useEffect } from "react"
 
 const PostAndCommentsPage: React.FC = () => {
-  const [isMounted, setIsMounted] = useState(false)
-
-  useEffect(() => {
-    setIsMounted(true)
-  }, [isMounted])
-
   const router = useRouter()
-  const postId = (router?.query?.id as string) ?? 1
+  const postId = (router?.query?.id as string) ?? "1"
 
-  const { loading, data, error } = usePostQuery({
+  const [getPost, { loading, data }] = usePostLazyQuery({
     variables: {
       postId: postId
     }
   })
 
-  if (error) return <div>error loading post</div>
-  if (loading) return null
+  useEffect(() => {
+    getPost()
+  }, [getPost])
 
-  const ViewPost = () => {
-    if (data && data.post) {
-      return (
-        <NewPost
-          key={`post-${data.post.id}-${data.post.title}`}
-          post={data.post}
-        />
-      )
-    }
-    return <div>Post Not Found.</div>
-  }
-
-  if (isMounted) {
+  if (data && data.post) {
     return (
-      <Stack spacing={2}>
-        <ViewPost />
-        <SubmitCommentForm postId={postId} />
-        <Box>Comments</Box>
-        <CommentsPageWithData postId={postId} />
-      </Stack>
+      <Skeleton isLoaded={!loading}>
+        <Stack spacing={2}>
+          <NewPost
+            key={`post-${data.post.id}-${data.post.title}`}
+            post={data.post}
+          />
+          <SubmitCommentForm postId={postId} />
+          <Box>Comments</Box>
+          <CommentsPageWithData postId={postId} />
+        </Stack>
+      </Skeleton>
     )
+  } else {
+    return <PostNotFound />
   }
-  return null
 }
 
 export default PostAndCommentsPage
