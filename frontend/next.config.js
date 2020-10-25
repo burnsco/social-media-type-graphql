36be1023b59a2fe230db7a789387af051b6f35ea
Corey Burns <coreymburns@gmail.com>
2020-10-25 16:47:04 -0400

    changed next config for bundle analyzer, register page seo added

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,9 @@
+const withPlugins = require("next-compose-plugins")
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true"
+})
+
+module.exports = withPlugins([[withBundleAnalyzer({})]])
 module.exports = {
   reactStrictMode: true,
   webpack(config, options) {
--- frontend/src/pages/register/index.tsx
+++ frontend/src/pages/register/index.tsx
@@ -1,5 +1,6 @@
 import { Wrapper } from "@/components/Layout/wrapper"
 import { ChakraField } from "@/components/shared/ChakraField"
+import SEO from "@/components/shared/seo"
 import { MeDocument, MeQuery, useRegisterMutation } from "@/generated/graphql"
 import { toErrorMap } from "@/utils/toErrorMap"
 import { Box, Button, useToast } from "@chakra-ui/core"
@@ -13,78 +14,81 @@ const RegisterPage: React.FC = () => {
   const [register] = useRegisterMutation()
 
   return (
-    <Wrapper variant="small">
-      <Formik
-        initialValues={{ username: "", email: "", password: "" }}
-        validationSchema={Yup.object().shape({
-          username: Yup.string().required("Required"),
-          email: Yup.string().required("Required"),
-          password: Yup.string().required("Required")
-        })}
-        onSubmit={async (values, { setErrors }) => {
-          const response = await register({
-            variables: {
-              data: {
-                username: values.username,
-                password: values.password,
-                email: values.email
-              }
-            },
-            update: (cache, { data }) => {
-              cache.writeQuery<MeQuery>({
-                query: MeDocument,
+    <>
+      <SEO title="Register" description="Register here to post on the site." />
+      <Wrapper variant="small">
+        <Formik
+          initialValues={{ username: "", email: "", password: "" }}
+          validationSchema={Yup.object().shape({
+            username: Yup.string().required("Required"),
+            email: Yup.string().required("Required"),
+            password: Yup.string().required("Required")
+          })}
+          onSubmit={async (values, { setErrors }) => {
+            const response = await register({
+              variables: {
                 data: {
-                  __typename: "Query",
-                  me: data?.register.user
+                  username: values.username,
+                  password: values.password,
+                  email: values.email
                 }
-              })
-            }
-          })
-          if (response.data?.register?.user) {
-            toast({
-              id: "success",
-              title: `Welcome ${response.data.register.user.username}!`,
-              description: "Your account was created successfully.",
-              status: "success",
-              duration: 9000,
-              isClosable: true
+              },
+              update: (cache, { data }) => {
+                cache.writeQuery<MeQuery>({
+                  query: MeDocument,
+                  data: {
+                    __typename: "Query",
+                    me: data?.register.user
+                  }
+                })
+              }
             })
-            router.push("/")
-          } else if (response.data?.register.errors) {
-            setErrors(toErrorMap(response.data.register.errors))
-          }
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <ChakraField name="email" placeholder="email" label="Email" />
-            <Box my="4">
-              <ChakraField
-                name="username"
-                placeholder="username"
-                label="Username"
-              />
-            </Box>
-            <Box my="4">
-              <ChakraField
-                name="password"
-                placeholder="password"
-                label="Password"
-                type="password"
-              />
-            </Box>
-            <Button
-              mt={4}
-              colorScheme="red"
-              type="submit"
-              isLoading={isSubmitting}
-            >
-              Submit
-            </Button>
-          </Form>
-        )}
-      </Formik>
-    </Wrapper>
+            if (response.data?.register?.user) {
+              toast({
+                id: "success",
+                title: `Welcome ${response.data.register.user.username}!`,
+                description: "Your account was created successfully.",
+                status: "success",
+                duration: 9000,
+                isClosable: true
+              })
+              router.push("/")
+            } else if (response.data?.register.errors) {
+              setErrors(toErrorMap(response.data.register.errors))
+            }
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <ChakraField name="email" placeholder="email" label="Email" />
+              <Box my="4">
+                <ChakraField
+                  name="username"
+                  placeholder="username"
+                  label="Username"
+                />
+              </Box>
+              <Box my="4">
+                <ChakraField
+                  name="password"
+                  placeholder="password"
+                  label="Password"
+                  type="password"
+                />
+              </Box>
+              <Button
+                mt={4}
+                colorScheme="red"
+                type="submit"
+                isLoading={isSubmitting}
+              >
+                Submit
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </Wrapper>
+    </>
   )
 }
 
